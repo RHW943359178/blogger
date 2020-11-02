cd974c120cb97097babc3d767405897eaf37f4d3
rhw943359178 <qiant@bsoft.com.cn>
2020-11-02 17:40:46 +0800

    model层category实现

---
+++ dao/db/category.go
@@ -0,0 +1,58 @@
+package db
+
+import (
+	"blogger/model"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+//	添加分类
+func InsertCategory(category *model.Category) (categoryId int64, err error) {
+	sqlStr := `insert into category(category_name, category_no) value(?, ?)`
+	result, err := db.Exec(sqlStr, category.CategoryName, category.CategoryNo)
+	if err != nil {
+		log.Fatal("exec sql failed, err: ", err)
+		return
+	}
+	categoryId, err = result.LastInsertId()
+	return
+}
+
+//	获取单个文章分类
+func GetCategoryById(id int64) (category *model.Category, err error) {
+	category = &model.Category{}
+	sqlStr := `select id, category_name, category_no from category where id = ?`
+	err = db.Get(category, sqlStr, id)
+	if err != nil {
+		log.Fatalln("get data failed, err: ", err)
+		return
+	}
+	return
+}
+
+//	获取多个文章分类
+func GetCategoryList(categoryIds []int64) (categories []*model.Category, err error) {
+	//	构建 sql 语句
+	sqlStr, args, err := sqlx.In(`select id, category_name, category_no from category where id in(?)`, categoryIds)
+	if err != nil {
+		log.Fatalln("sqlx in failed, err: ", err)
+		return
+	}
+	//	语句
+	err = db.Select(&categories, sqlStr, args...)
+	if err != nil {
+		return
+	}
+	return
+}
+
+//	获取所有文章分类
+func GetAllCategory() (categories []*model.Category, err error) {
+	sqlStr := `select id, category_name, category_no from category order by category_no asc ?`
+	err = db.Get(categories, sqlStr, id)
+	if err != nil {
+		log.Fatalln("get data failed, err: ", err)
+		return
+	}
+	return
+}
--- dao/db/db.go
+++ dao/db/db.go
@@ -1,5 +1,25 @@
 package db
 
 import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/jmoiron/sqlx"
 )
+
+var (
+	db *sqlx.DB
+)
+
+func Init(dns string) (err error) {
+	db, err = sqlx.Open("mysql", dns)
+	if err != nil {
+		fmt.Println("connect to database failed, err: ", err)
+		return
+	}
+	//	查看是否连接成功
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+	return nil
+}
